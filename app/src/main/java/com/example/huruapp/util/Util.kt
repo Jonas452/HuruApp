303371eb2b0929a920cc6fd6145bc4232e911be3
Jonas Jordão de Macêdo <jonasjordao452@gmail.com>
2019-11-27 16:06:03 -0300

    Adding format date and load image in util function

--- app/src/main/java/com/example/huruapp/util/Util.kt
+++ app/src/main/java/com/example/huruapp/util/Util.kt
@@ -1,10 +1,44 @@
 package com.example.huruapp.util
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.ConnectivityManager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import java.net.URL
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun isOnline(context: Context): Boolean {
     val connectivityManager=context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val networkInfo=connectivityManager.activeNetworkInfo
     return  networkInfo!=null && networkInfo.isConnected
+}
+
+fun formatDate(date: String): String {
+    val outputFormat: DateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+    val inputFormat: DateFormat =
+        SimpleDateFormat("yyyy-MM-dd", Locale.US)
+
+    val inputText = date
+    val dateOutput: Date = inputFormat.parse(inputText)
+    val outputText: String = outputFormat.format(dateOutput)
+
+    return outputText
+}
+
+fun loadImage(url: String): Bitmap = runBlocking {
+    val bmp: Bitmap
+    val funAsync = GlobalScope.async {
+        val url = URL(url)
+        val bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        return@async bmp
+    }
+
+    bmp = funAsync.await()
+
+    return@runBlocking bmp
 }
